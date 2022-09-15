f79d83cd403e3e32ad4bbb546f824a8bb7643d23
Justin Welsh <justinwelsh33@gmail.com>
2022-09-15 13:58:19 -0500

    added tailwind CSS dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-4xl font-bold underline pb-8">
+          Hello world!
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
